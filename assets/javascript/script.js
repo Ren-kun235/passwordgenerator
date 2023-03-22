743ebe2ae4dddea6a49670bc20f2551adb3b140e
Ren-Kun235 <changpobzeb651@gmail.com>
2023-03-21 20:33:57 -0500

    added comments

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var lowerCase {
+  'a'
+}
+
+// function generatePassword() {
+
+// }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +20,15 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// define some arrays of characters such as upperCase/lowerCase/special characters/numbers
+// prompts to ask user how many characters (8-128)
+// confirm selection of characters being used
+// as they confirm, concat selected arrays to new arrays
+// create function to fetch a random # and save to a variable(var)
+// use that # as index to a new array
+
+// math.Random generates #(s) a randomized number(s) between 0-1
+// math.Floor rounds 1.2345 to an integer such as (1)
+// newArr[] the # that is in []'s (repeated step)
