29eda99b4fa7e865e3a3b07be6b7e51c778852aa
Ren-Kun235 <changpobzeb651@gmail.com>
2023-03-23 19:09:20 -0500

    added numbers/special characters/and lower/upper case

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,13 +1,98 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowerCase {
-  'a'
-}
+var lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+]
+var upperCase = [ 
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N", 
+  "O", 
+  "P", 
+  "Q", 
+  "R", 
+  "S", 
+  "T", 
+  "U", 
+  "V", 
+  "W", 
+  "X",
+  "Y", 
+  "Z" 
+]
 
-// function generatePassword() {
+var numbers = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "0"
+]
+var specialChar = [
+  "`",
+  "~",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "-",
+  "_",
+  "=",
+  "+",
+  ",",
+  "<",
+  ".",
+  ">",
+  "?"
+]
 
-// }
+
 
 // Write password to the #password input
 function writePassword() {
@@ -32,3 +117,5 @@ generateBtn.addEventListener("click", writePassword);
 // math.Random generates #(s) a randomized number(s) between 0-1
 // math.Floor rounds 1.2345 to an integer such as (1)
 // newArr[] the # that is in []'s (repeated step)
+
+// var abcd = [] <- brackets define the array wanted.
